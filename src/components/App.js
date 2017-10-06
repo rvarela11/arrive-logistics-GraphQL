dc696e40284f3286780c30afb15ab33a3d621eef
Robert Varela <robert.varela@rvarela-ret.local>
2017-10-05 21:09:29 -0500

    Completed the Searchbox component and connected it to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+//Actions
 import { addMessage, deleteMessage } from '../actions/index';
+
+//Components
+import Searchbox from './Searchbox';
 
 class App extends Component {
 
   render() {
-    return <div className="mainContainer">
+    return <div>
       <header className="header-image"></header>
+      <div className="main-container container">
+        <Searchbox inputValue={this.handleSearchByCity}/>
+      </div>
       <button onClick={this.handleSearchCityClick}>Click</button>
     </div>
   }
-  handleSearchCityClick = () => {
-    const URL = 'http://arrive-interview-api.azurewebsites.net/api/carriers';
 
-    fetch(URL)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  handleSearchByCity = (city) => {
+    console.log(city);
+
+    // const URL = `http://arrive-interview-api.azurewebsites.net/api/carriers/${city}`;
+    //
+    // fetch(URL)
+    // .then((response) => {
+    //   return response.json();
+    // })
+    // .then((data) => {
+    //   console.log(data);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
   }
 }
 
---
+++ src/components/Searchbox.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Searchbox extends Component {
+
+  state = {
+    inputValue: ''
+  }
+
+  render() {
+    return <div className="searchbox-container container">
+      <form onSubmit={this.handleSubmit}>
+          <input id="searchbox_form_input" type="text"  placeholder="Search by city" onChange={this.handleChange}/>
+      </form>
+    </div>
+  }
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.inputValue(this.state.inputValue);
+
+    // Clean inputValue state and input value
+    this.setState({ inputValue: ''});
+    const searchbox_form_input = document.getElementById("searchbox_form_input");
+    searchbox_form_input.value = "";
+  }
+
+}
+
+export default Searchbox;
