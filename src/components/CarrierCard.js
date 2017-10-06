7773ed639ba49be0303ef7f1d3d1d797a8773ea9
Robert Varela <robert.varela@rvarela-ret.local>
2017-10-06 12:49:08 -0500

    started creating the CarrierCard component. Added hide/show logic

---
+++ src/components/CarrierCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CarrierCard extends Component {
+
+  render() {
+    return <div>
+      <h2>Card</h2>
+    </div>
+  }
+
+}
+
+export default CarrierCard;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+//Components
+import CarrierCard from './CarrierCard';
+
 class SearchResults extends Component {
 
+  state = {
+    show: false
+  }
+
   render() {
-    return <div className="search-results-container container">
+    return <div>
+    <div className="search-results-container container" onClick={this.handleClick}>
       <div className="search-results__name">
         <h5>{this.props.name}</h5>
       </div>
@@ -13,6 +21,24 @@ class SearchResults extends Component {
         )}
       </div>
     </div>
+      {this.state.show && <CarrierCard/>}
+    </div>
+  }
+
+  handleClick = () => {
+    this.setState({show: !this.state.show});
+    // let URL = `http://arrive-interview-api.azurewebsites.net/api/carrierDetails/${this.props.id}`;
+    //
+    // fetch(URL)
+    // .then((response) => {
+    //   return response.json();
+    // })
+    // .then((data) => {
+    //   console.log(data);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
   }
 
 }
